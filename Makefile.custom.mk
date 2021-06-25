34db4315c0c17906a172891b9e9e7ff559979e17
Chris <github.account@chrigel.net>
2021-06-25 15:18:21 +0200

    Fix make docs

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -1,2 +1,2 @@
-test: .compile ## Test component with a provider set by "provider=..."
+test: .compile ## Test component with a provider set by "instance=..."
 	cd tests/ && go test ./$(instance)/... -count=1
