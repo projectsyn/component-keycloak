98f391c8a0d6487ebe2bc4ee170edb107f174cea
Simon Gerber <simon.gerber@vshn.ch>
2021-08-05 14:34:29 +0200

    Add test to check that the Postgres subchart isn't rendered in external DB mode

---
+++ tests/external/postgresql_helmchart_not_rendered_test.go
@@ -0,0 +1,12 @@
+package external
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Postgresql_Helmchart_Not_Rendered(t *testing.T) {
+	subChartDir := testPath+"/01_keycloak_helmchart/keycloak/charts"
+	require.NoDirExists(t, subChartDir)
+}
