12ed54681e3d981d9e6b1ef7c5755942eb547dfd
Manuel Hutter <manuel@hutter.io>
2021-10-30 17:09:35 +0200

    Document & test multiple volumeMounts
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- tests/builtin/statefulset_keycloak_test.go
+++ tests/builtin/statefulset_keycloak_test.go
@@ -30,6 +30,10 @@ func Test_Keycloak_StatefulSet_Secrets(t *testing.T) {
 
 	envFrom := subject.Spec.Template.Spec.Containers[0].EnvFrom
 	assert.Equal(t, expectedDbSecretName, envFrom[1].SecretRef.Name)
+}
+
+func Test_Keycloak_StatefulSet_InitContainers(t *testing.T) {
+	subject := common.DecodeStatefulsetV1(t, testPath+"/01_keycloak_helmchart/keycloak/templates/statefulset.yaml")
 
 	assert.Len(t, subject.Spec.Template.Spec.InitContainers, 2)
 	for _, c := range subject.Spec.Template.Spec.InitContainers {
@@ -37,24 +41,50 @@ func Test_Keycloak_StatefulSet_Secrets(t *testing.T) {
 			assert.Equal(t, "company/keycloak-theme:v1.0.0", c.Image)
 		}
 	}
+}
 
-	assert.Len(t, subject.Spec.Template.Spec.Volumes, 4)
-	containsTest := false
+func Test_Keycloak_StatefulSet_Volumes(t *testing.T) {
+	subject := common.DecodeStatefulsetV1(t, testPath+"/01_keycloak_helmchart/keycloak/templates/statefulset.yaml")
+
+	volumes := make(map[string]int)
 	for _, v := range subject.Spec.Template.Spec.Volumes {
-		if v.Name == "test" {
-			containsTest = true
-		}
+		volumes[v.Name]++
 	}
-	assert.True(t, containsTest)
 
-	assert.Len(t, subject.Spec.Template.Spec.Containers[0].VolumeMounts, 4)
-	containsTest = false
-	for _, v := range subject.Spec.Template.Spec.Containers[0].VolumeMounts {
-		if v.Name == "test" {
-			containsTest = true
+	// from chart
+	assert.Equal(t, 1, volumes["startup"])
+
+	// from defaults.yml
+	assert.Equal(t, 1, volumes["db-certs"])
+	assert.Equal(t, 1, volumes["keycloak-tls"])
+
+	// from test inventory
+	assert.Equal(t, 1, volumes["theme"])
+}
+func Test_Keycloak_StatefulSet_VolumeMounts(t *testing.T) {
+	subject := common.DecodeStatefulsetV1(t, testPath+"/01_keycloak_helmchart/keycloak/templates/statefulset.yaml")
+	container := subject.Spec.Template.Spec.Containers[0]
+
+	// ensure we have the correct container
+	assert.Equal(t, "keycloak", container.Name)
+
+	assert.Len(t, container.VolumeMounts, 4)
+	volumeMounts := make(map[string]int)
+	for _, v := range container.VolumeMounts {
+		volumeMounts[v.Name]++
+
+		if v.Name == "theme" {
 			assert.Equal(t, "/opt/test", v.MountPath)
 		}
 	}
-	assert.True(t, containsTest)
 
+	// from chart
+	assert.Equal(t, 1, volumeMounts["startup"])
+
+	// from defaults.yml
+	assert.Equal(t, 1, volumeMounts["db-certs"])
+	assert.Equal(t, 1, volumeMounts["keycloak-tls"])
+
+	// from test inventory
+	assert.Equal(t, 1, volumeMounts["theme"])
 }
--- tests/external/statefulset_keycloak_test.go
+++ tests/external/statefulset_keycloak_test.go
@@ -24,8 +24,47 @@ func Test_Keycloak_StatefulSet_Secrets(t *testing.T) {
 
 	envFrom := subject.Spec.Template.Spec.Containers[0].EnvFrom
 	assert.Equal(t, expectedDbSecretName, envFrom[1].SecretRef.Name)
-
 	assert.Len(t, subject.Spec.Template.Spec.InitContainers, 0)
-	assert.Len(t, subject.Spec.Template.Spec.Volumes, 3)
-	assert.Len(t, subject.Spec.Template.Spec.Containers[0].VolumeMounts, 3)
+}
+
+func Test_Keycloak_StatefulSet_Volumes(t *testing.T) {
+	subject := common.DecodeStatefulsetV1(t, testPath+"/01_keycloak_helmchart/keycloak/templates/statefulset.yaml")
+
+	volumes := make(map[string]int)
+	for _, v := range subject.Spec.Template.Spec.Volumes {
+		volumes[v.Name]++
+	}
+
+	// from chart
+	assert.Equal(t, 1, volumes["startup"])
+
+	// from defaults.yml
+	assert.Equal(t, 1, volumes["db-certs"])
+	assert.Equal(t, 1, volumes["keycloak-tls"])
+
+	// from test inventory
+	assert.Equal(t, 1, volumes["themes"])
+}
+func Test_Keycloak_StatefulSet_VolumeMounts(t *testing.T) {
+	subject := common.DecodeStatefulsetV1(t, testPath+"/01_keycloak_helmchart/keycloak/templates/statefulset.yaml")
+	container := subject.Spec.Template.Spec.Containers[0]
+
+	// ensure we have the correct container
+	assert.Equal(t, "keycloak", container.Name)
+
+	assert.Len(t, container.VolumeMounts, 6)
+	volumeMounts := make(map[string]int)
+	for _, v := range container.VolumeMounts {
+		volumeMounts[v.Name]++
+	}
+
+	// from chart
+	assert.Equal(t, 1, volumeMounts["startup"])
+
+	// from defaults.yml
+	assert.Equal(t, 1, volumeMounts["db-certs"])
+	assert.Equal(t, 1, volumeMounts["keycloak-tls"])
+
+	// from test inventory
+	assert.Equal(t, 3, volumeMounts["themes"])
 }
