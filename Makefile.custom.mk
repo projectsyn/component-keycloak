f6a22686dfdbcb61d1f5cebd7e8b793f72acbe6e
Simon Gerber <simon.gerber@vshn.ch>
2021-11-09 16:39:24 +0100

    Refactor go test config to not be eaten by modulesync

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -1,5 +1,11 @@
 # Configure instance alias for commodore component compile
 commodore_args += --alias $(instance)
 
-test: .compile ## Test component with a provider set by "instance=..."
-	cd tests/ && go test ./$(instance)/... -count=1
+# Note: if redefine target `test` here it doesn't have the desired effect, as
+# the target `test` defined in `Makefile` is defined "later" than the target
+# `test` here, since we include the additional Makefile quite early in the
+# main `Makefile`.
+.PHONY: gotest
+gotest: commodore_args += -f tests/$(instance).yml
+gotest: .compile ## Test component with a provider set by "instance=..."
+	cd tests/ && go test -count=1 ./$(instance)
