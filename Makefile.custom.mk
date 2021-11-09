4a67982ba00c4228a887614f35f02cf0b69fbe93
Simon Gerber <simon.gerber@vshn.ch>
2021-11-09 16:28:24 +0100

    Configure component compile alias in `Makefile.custom.mk`

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -1,2 +1,5 @@
+# Configure instance alias for commodore component compile
+commodore_args += --alias $(instance)
+
 test: .compile ## Test component with a provider set by "instance=..."
 	cd tests/ && go test ./$(instance)/... -count=1
