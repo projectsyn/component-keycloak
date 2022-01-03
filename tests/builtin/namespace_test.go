98916627bdd5a791238f3481d3d22c06748fd515
Simon Gerber <simon.gerber@vshn.ch>
2022-01-03 15:27:45 +0100

    Update tests

---
+++ tests/builtin/namespace_test.go
@@ -0,0 +1,23 @@
+package builtin
+
+import (
+	"testing"
+
+	"github.com/projectsyn/component-keycloak/common"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	expectedNsLabels = map[string]string{
+		"SYNMonitoring": "main",
+		"name":          "syn-builtin",
+		"test":          "testing",
+	}
+)
+
+func Test_NamespaceLabels(t *testing.T) {
+	ns := common.DecodeNamespace(t, testPath+"/00_namespace.yaml")
+	require.NotEmpty(t, ns.Labels)
+	assert.Equal(t, ns.Labels, expectedNsLabels)
+}
