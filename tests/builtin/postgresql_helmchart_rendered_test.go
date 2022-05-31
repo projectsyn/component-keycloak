95d3bc6029bfc7f2bf518ec8ea07ba5dbd0347ee
Gabriel Mainberger <gabriel.mainberger@vshn.net>
2022-05-31 16:37:44 +0200

    Make postgres helm chart conditional again

--- tests/builtin/postgresql_helmchart_rendered_test.go
+++ tests/builtin/postgresql_helmchart_rendered_test.go
@@ -1,4 +1,4 @@
-package external
+package builtin
 
 import (
 	"testing"
@@ -19,14 +19,14 @@ func Test_Postgresql_Helmchart_Not_Rendered(t *testing.T) {
 	subChartDir := testPath + "/01_keycloak_helmchart/postgresql/templates"
 
 	StatefulSetFileSize := FileSize(t, subChartDir+"/statefulset.yaml")
-	assert.Equal(t, StatefulSetFileSize, int64(0))
+	assert.NotEqual(t, StatefulSetFileSize, int64(0))
 
 	ServiceFileSize := FileSize(t, subChartDir+"/svc.yaml")
-	assert.Equal(t, ServiceFileSize, int64(0))
+	assert.NotEqual(t, ServiceFileSize, int64(0))
 
 	ServiceHeadlessFileSize := FileSize(t, subChartDir+"/svc-headless.yaml")
-	assert.Equal(t, ServiceHeadlessFileSize, int64(0))
+	assert.NotEqual(t, ServiceHeadlessFileSize, int64(0))
 
 	NetworkPolicyFileSize := FileSize(t, subChartDir+"/networkpolicy.yaml")
-	assert.Equal(t, NetworkPolicyFileSize, int64(0))
+	assert.NotEqual(t, NetworkPolicyFileSize, int64(0))
 }
