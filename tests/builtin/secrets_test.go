6b830925aeabca7d1454d63d827f0f0017480506
Gabriel Mainberger <gabriel.mainberger@vshn.net>
2022-07-22 15:20:07 +0200

    Ajust postgresql(Database|Username) to auth.(database|username)
    Required by the helm chart 11.6.18

--- tests/builtin/secrets_test.go
+++ tests/builtin/secrets_test.go
@@ -24,6 +24,6 @@ func Test_Database_Secret_DefaultParameters(t *testing.T) {
 	expected := "t-silent-test-1234/c-green-test-1234/builtin/db-password"
 	assert.Len(t, data, 4)
 	assert.Equal(t, expected, data["KC_DB_PASSWORD"])
-	assert.Equal(t, expected, data["postgresql-password"])
-	assert.Equal(t, expected, data["postgresql-postgres-password"])
+	assert.Equal(t, expected, data["postgres-password"])
+	assert.Equal(t, expected, data["password"])
 }
