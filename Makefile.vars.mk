dc218d0f5805953de61c22b2271464b4ed0cdff6
Simon Gerber <simon.gerber@vshn.ch>
2022-10-27 19:46:55 +0200

    Update from template
    Template version: main (d3af334)

--- Makefile.vars.mk
+++ Makefile.vars.mk
@@ -53,8 +53,7 @@ GOLDEN_FILES    ?= $(shell find tests/golden/$(instance) -type f)
 
 KUBENT_FILES    ?= $(shell echo "$(GOLDEN_FILES)" | sed 's/ /,/g')
 KUBENT_ARGS     ?= -c=false --helm2=false --helm3=false -e
-# Use our own kubent image until the upstream image is available
-KUBENT_IMAGE    ?= docker.io/projectsyn/kubent:latest
+KUBENT_IMAGE    ?= ghcr.io/doitintl/kube-no-trouble:latest
 KUBENT_DOCKER   ?= $(DOCKER_CMD) $(DOCKER_ARGS) $(root_volume) --entrypoint=/app/kubent $(KUBENT_IMAGE)
 
 instance ?= builtin
